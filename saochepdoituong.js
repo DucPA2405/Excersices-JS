415b89d28dad480a0a0164de5205e89f6a727cb1
PSuBlack <anhducmk2@gmail.com>
2024-07-19 09:31:19 +0700

    update sao chép đối tượng

--- saochepdoituong.js
+++ saochepdoituong.js
@@ -1,24 +1,40 @@
-function cloneObject(obj) {
-  var clone = {};
-  for (var i in obj) {
-    if (obj[i] != null && typeof obj[i] == "object")
-      clone[i] = cloneObject(obj[i]);
-    else clone[i] = obj[i];
+function deepClone(obj) {
+  // Nếu đối tượng không phải là object hoặc là null, không cần sao chép
+  if (obj === null || typeof obj !== "object") {
+    return obj;
   }
+
+
+  // Tạo một bản sao mới của đối tượng
+  let clone = {};
+
+
+  // Lặp qua tất cả các thuộc tính của đối tượng
+  for (let key in obj) {
+    // Kiểm tra xem thuộc tính đó có phải là thuộc tính riêng của đối tượng hay không
+    if (obj.hasOwnProperty(key)) {
+      // Nếu là một đối tượng, gọi đệ quy để tạo ra một bản sao sâu
+      if (typeof obj[key] === "object") {
+        clone[key] = deepClone(obj[key]);
+      } else {
+        // Nếu là một giá trị nguyên thủy, sao chép giá trị đó sang bản sao mới
+        clone[key] = obj[key];
+      }
+    }
+  }
+
+
+  // Trả về bản sao mới
   return clone;
 }
 
-// Cách sử dụng
-const checkStaff = {
-  debug: true,
-  position: "Nhân viên",
-  jobDetail: "Dev",
-  data: {
-    position: 1,
-  },
-};
 
-const checkStaffAgain = cloneObject(checkStaff);
-checkStaffAgain["debug"] = false;
-checkStaffAgain["data"]["position"] = 2;
-console.log(checkStaff);
+const obj1 = { a: 1, b: 2, c: { d: 3 } };
+const obj2 = deepClone(obj1);
+
+
+obj2.c.d = 4;
+
+
+console.log(obj1); // { a: 1, b: 2, c: { d: 3 } }
+console.log(obj2); // { a: 1, b: 2, c: { d: 4 } }
